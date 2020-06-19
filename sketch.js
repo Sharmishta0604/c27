9a1c6698d081b72fc07e498a5747221389f0f92e
Sharmishta0604 <send2sonali@gmail.com>
2020-06-19 19:58:40 +0530

    chain made

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var newLog;
+var newLog,restrainedlog;
+var chain;
+var bird;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,8 +37,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
     bird = new Bird(100,100);
+    chain=new Chain(bird.body,newLog.body);
+    
 
 }
 
@@ -59,6 +63,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    chain.display();
 
     bird.display();
     platform.display();
